2257af595ef64d38df17daed4ca7e40590316930
jessethach <jessethach@gmail.com>
2015-10-13 10:22:08 -0700

    Pivoted project to make the voting aspect easier to track

---
+++ app.js
@@ -0,0 +1,59 @@
+//Creating question constructor
+
+var imageArray = [];
+
+var seaQuestions = function() {
+  this.src = src;
+  this.title = title;
+  this.neighbor = neighbor;
+
+  function askQuestions(img, name, movie) {
+    var sea = new seaQuestions(img, name, movie);
+    imageArray.push(sea);
+  }
+}
+
+
+function randomImg () {
+  var random = randomNumber();
+  var randomPhoto = (imageArray[random]);
+  //populating the DOM with an image
+  document.getElementsByTagName('').src =randomPhoto1["src"];
+  var title1 document.getElementById("photoName");
+  title1.appendChild(document.createTextNode(randomPhoto1["title"]));
+  var neighbor.appendChild(document.createTextNode(randomPhoto1["neighbor"]));
+  neighbor.appendChild(document.createTextNode(randomPhoto1["neighbor"]));
+}
+
+// Hard wired answers
+
+for(var i = 0; i < 2; i++) {
+  randomImg(i);
+  //duplicate photo error correction
+}
+
+var randNum = function () {
+  return Math.floor(Math.random()) * );
+}
+
+//Store Event
+var answerSubmit = function(e) {
+  e.preventDefault();
+
+  //Collect number of yes clicks ascoiated with 'yes' and 'no'
+  if (yesAnswer) {
+    yesAnswers++,
+  }
+  //Collect number of no clicks
+  else if (noAnswers) {
+    noAnswers++,
+  }
+
+  //Run a for loop to restart code in event
+  for (number of photos in an array, run this code){
+
+  }
+
+}
+
+
